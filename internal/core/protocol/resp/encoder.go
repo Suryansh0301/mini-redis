6cd235a0b9bc0b798200300a9531a4483b75e674
Suryansh0301 <02.suryanshgoel@gmail.com>
2026-03-18 08:26:22 +0530

    made changes to encoder made it more readable

--- internal/core/protocol/resp/encoder.go
+++ internal/core/protocol/resp/encoder.go
@@ -2,42 +2,38 @@ package resp
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/suryansh0301/mini-redis/internal/core/common"
 	"github.com/suryansh0301/mini-redis/internal/enums"
 )
 
-/*
+var encoderHandler map[enums.RespType]func(value common.RespValue) []byte
 
-type RespValue struct {
-	Type   enums.RespType
-	Str    string
-	Int    int64
-	Array  []*RespValue
-	IsNull bool
+func init() {
+	encoderHandler = make(map[enums.RespType]func(value common.RespValue) []byte)
+
+	encoderHandler[enums.SimpleStringRespType] = func(value common.RespValue) []byte {
+		response := fmt.Sprintf("+" + value.Str + "\r\n")
+		return []byte(response)
+	}
+
+	encoderHandler[enums.IntRespType] = func(value common.RespValue) []byte {
+		response := fmt.Sprintf(":" + strconv.FormatInt(value.Int, 10) + "\r\n")
+		return []byte(response)
+	}
+
+	encoderHandler[enums.BulkStringRespType] = func(value common.RespValue) []byte {
+		response := fmt.Sprintf("$" + (value.Str) + "\r\n" + value.Str + "\r\n")
+		return []byte(response)
+	}
+
+	encoderHandler[enums.ErrorRespType] = func(value common.RespValue) []byte {
+		response := fmt.Sprintf("-" + value.Str + "\r\n")
+		return []byte(response)
+	}
 }
 
-type RespType int
-
-const (
-	SimpleStringRespType RespType = iota
-	BulkStringRespType
-	IntRespType
-	ArrayRespType
-	ErrorRespType
-)
-
-*/
-
 func Encoder(resp common.RespValue) []byte {
-	if resp.Type == enums.SimpleStringRespType {
-		response := fmt.Sprintf("+" + resp.Str + "\r\n")
-		return []byte(response)
-	} else if resp.Type == enums.IntRespType {
-
-	} else if resp.Type == enums.BulkStringRespType {
-
-	} else if resp.Type == enums.ErrorRespType {
-	}
-	return []byte{}
+	return encoderHandler[resp.Type](resp)
 }
